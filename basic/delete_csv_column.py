4dd1feeded991dbeb24bd2c4ca4bbe3c52c039ae
Lingcao Huang <huanglingcao@gmail.com>
2017-04-24 17:32:21 +0800

    add script to edit csv file

--- basic/delete_csv_column.py
+++ basic/delete_csv_column.py
@@ -19,13 +19,21 @@ def delete_fouth_coloumn_csv(csv_path):
         reader = csv.reader(f)
         input_list = map(tuple, reader)
         f.close()
-
-    save_csv=open('test.csv','wb')
+    save_path = io_function.get_name_by_adding_tail(csv_path,'modify')
+    save_csv=open(save_path,'wb')
     csv_writer = csv.writer(save_csv)
 
+    number = 1
     for line in input_list:
-        save_ele = line[0:3]
+        # if number ==1:
+        #     number = number+1
+        #     continue
+        number = number + 1
+        save_ele = list(line)
+        # line[3] = str(1)
+        save_ele[3] = str(1)
         csv_writer.writerows([save_ele])
+        # csv_writer.writerows([line])
 
     save_csv.close()
 
@@ -35,8 +43,8 @@ def delete_fouth_coloumn_csv(csv_path):
 
 def main(options, args):
 
-    # path = args[0]
-    path = 'result_buildings.csv'
+    path = args[0]
+    # path = 'result_buildings.csv'
     delete_fouth_coloumn_csv(path)
 
     pass
@@ -49,5 +57,8 @@ if __name__=='__main__':
     parser = OptionParser(usage=usage, version="1.0 2017-4-24")
 
     (options, args) = parser.parse_args()
+    if len(args) <1:
+        parser.print_help()
+        exit(1)
 
     main(options, args)
