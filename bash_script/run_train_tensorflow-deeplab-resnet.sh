4a255c3ee8616ee7ffac16b238a43d208981539d
Lingcao Huang <huanglingcao@gmail.com>
2017-05-01 21:46:40 +0800

    add bash file

--- bash_script/run_train_tensorflow-deeplab-resnet.sh
+++ bash_script/run_train_tensorflow-deeplab-resnet.sh
@@ -7,7 +7,7 @@ dataDir=
 data_list=${HOME}/Data/aws_SpaceNet/deeplab_exper/spacenet_rgb_aoi_2/list/train_aug.txt
 num_classes=2
 
-
+pre_train_model=${HOME}/Data/aws_SpaceNet/deeplab_exper/spacenet_rgb_aoi_2/model/deeplab_resnet_init.ckpt
 
 #run script with python3 and tensorflow
-python ${train_script} --data-dir ${dataDir} --data-list ${data_list} --num-classes ${num_classes} --ignore-label 255 --not-restore-last
+python ${train_script} --data-dir ${dataDir} --data-list ${data_list} --restore-from  --num-classes ${num_classes} --ignore-label 255 --not-restore-last
